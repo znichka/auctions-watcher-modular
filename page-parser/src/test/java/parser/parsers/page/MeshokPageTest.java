b02053421fabec72e1abf317a541b70bf448cf5f
Ekaterina Alay <catherine.alay@gmail.com>
2024-04-21 13:57:12 +0200

    fix: make non-en links

--- page-parser/src/test/java/parser/parsers/page/MeshokPageTest.java
+++ page-parser/src/test/java/parser/parsers/page/MeshokPageTest.java
@@ -38,19 +38,19 @@ public class MeshokPageTest {
 //    }
     @Test
     public void getAllItems() {
-        String url = "https://meshok.net/en/listing?a_o=15&city_id=32&good=16158&sort=beg_date&way=desc&reposted=N";
+        String url = "https://meshok.net/listing?a_o=15&city_id=32&good=16158&sort=beg_date&way=desc&reposted=N";
         assertTrue(meshokPageParser.getAllItems(url).size() > 0);
 
     }
     @Test
     public void getAllItemsSearch() {
-        String url = "https://meshok.net/en/listing?a_o=15&good=14299&reposted=N&search=%D0%B1%D1%83%D1%81%D1%8B&sort=beg_date&way=desc&reposted=N";
+        String url = "https://meshok.net/listing?a_o=15&good=14299&reposted=N&search=%D0%B1%D1%83%D1%81%D1%8B&sort=beg_date&way=desc&reposted=N";
         assertTrue(meshokPageParser.getAllItems(url).size() > 0);
 
     }
     @Test
     public void getNoItemsSearch() {
-        String url = "https://meshok.net/en/listing?a=1&a_o=2&a_o=15&good=109&search=blablabla&pp=48&sort=beg_date&way=desc&reposted=N";
+        String url = "https://meshok.net/listing?a=1&a_o=2&a_o=15&good=109&search=blablabla&pp=48&sort=beg_date&way=desc&reposted=N";
         assertEquals(0, meshokPageParser.getAllItems(url).size());
 
     }
